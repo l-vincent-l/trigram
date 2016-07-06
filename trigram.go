24b189af11a8c747fb64eeca149ff29925f2c86c
l-vincent-l <lara.vincent@gmail.com>
2016-07-06 22:31:53 +0200

    Simplify a bit

--- trigram.go
+++ trigram.go
@@ -14,9 +14,6 @@ func (d docList) Less(i, j int) bool { return d[i] < d[j] }
 type IndexResult struct {
 	//Save all trigram mapping docID
 	DocIDs map[int]bool
-
-	//Save all trigram appear time for trigram deletion
-	Freq map[int]int
 }
 
 // Extract one string to trigram list
@@ -42,78 +39,35 @@ type TrigramIndex struct {
 
 	//it represent and document incremental index
 	maxDocID int
-
-	//it include currently all the doc list, it will be used when query string length less than 3
-	docIDsMap map[int]bool
 }
 
 //Create a new trigram indexing
 func NewTrigramIndex() *TrigramIndex {
 	t := new(TrigramIndex)
 	t.TrigramMap = make(map[Trigram]IndexResult)
-	t.docIDsMap = make(map[int]bool)
 	return t
 }
 
 //Add new document into this trigram index
 func (t *TrigramIndex) Add(doc string) int {
 	newDocID := t.maxDocID + 1
-	trigrams := ExtractStringToTrigram(doc)
-	for _, tg := range trigrams {
-		var mapRet IndexResult
-		var exist bool
-		if mapRet, exist = t.TrigramMap[tg]; !exist {
-			//New doc ID handle
-			mapRet = IndexResult{}
-			mapRet.DocIDs = make(map[int]bool)
-			mapRet.Freq = make(map[int]int)
-			mapRet.DocIDs[newDocID] = true
-			mapRet.Freq[newDocID] = 1
+	for _, tg := range ExtractStringToTrigram(doc) {
+        mapRet, exist := t.TrigramMap[tg]
+		if !exist {
+			t.TrigramMap[tg] = IndexResult{
+                map[int]bool{newDocID: true},
+            }
 		} else {
 			//trigram already exist on this doc
-			if _, docExist := mapRet.DocIDs[newDocID]; docExist {
-				mapRet.Freq[newDocID] = mapRet.Freq[newDocID] + 1
-			} else {
-				//tg eixist but new doc id is not exist, add it
+			if _, docExist := mapRet.DocIDs[newDocID]; !docExist {
 				mapRet.DocIDs[newDocID] = true
-				mapRet.Freq[newDocID] = 1
 			}
+            t.TrigramMap[tg] = mapRet
 		}
-		//Store or Add  result
-		t.TrigramMap[tg] = mapRet
 	}
 
 	t.maxDocID = newDocID
-	t.docIDsMap[newDocID] = true
 	return newDocID
-}
-
-//Delete a doc from this trigram indexing
-func (t *TrigramIndex) Delete(doc string, docID int) {
-	trigrams := ExtractStringToTrigram(doc)
-	for _, tg := range trigrams {
-		if obj, exist := t.TrigramMap[tg]; exist {
-			if freq, docExist := obj.Freq[docID]; docExist && freq > 1 {
-				obj.Freq[docID] = obj.Freq[docID] - 1
-			} else {
-				//need remove trigram from such docID
-				delete(obj.Freq, docID)
-				delete(obj.DocIDs, docID)
-			}
-
-			if len(obj.DocIDs) == 0 {
-				//this object become empty remove this.
-				delete(t.TrigramMap, tg)
-				//TODO check if some doc id has no tg remove
-			} else {
-				//update back since there still other doc id exist
-				t.TrigramMap[tg] = obj
-			}
-		} else {
-			//trigram not exist in map, leave
-			return
-		}
-	}
 }
 
 //This function help you to intersect two map
@@ -140,9 +94,6 @@ func IntersectTwoMap(IDsA, IDsB map[int]bool) map[int]bool {
 //Query a target string to return the doc ID
 func (t *TrigramIndex) Query(doc string) docList {
 	trigrams := ExtractStringToTrigram(doc)
-	if len(trigrams) == 0 {
-		return t.getAllDocIDs()
-	}
 
 	//Find first trigram as base for intersect
 	retObj, exist := t.TrigramMap[trigrams[0]]
@@ -174,8 +125,4 @@ func getMapToSlice(inMap map[int]bool) docList {
 	}
 	sort.Sort(retSlice)
 	return retSlice
-}
-
-func (t *TrigramIndex) getAllDocIDs() docList {
-	return getMapToSlice(t.docIDsMap)
 }
